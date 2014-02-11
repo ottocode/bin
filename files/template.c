4a75d611800a3cb7c86a9ae4ce5a5e1eeea4df51
Nicholas Otto <nicholas.d.otto@gmail.com>
2014-02-11 10:15:33 -0800

    Finally adding the c template

---
+++ files/template.c
@@ -0,0 +1,13 @@
+/** A small template for C programs.
+ * Good for when I just want to write a quick test
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+
+    return 0;
+}
