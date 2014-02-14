f717a87e53edbda0ca254358e3d90d4ab1902392
Nicholas Otto <nicholas.d.otto@gmail.com>
2014-02-14 14:39:52 -0800

    Better C template, standardized commenting scheme

--- files/template.c
+++ files/template.c
@@ -1,12 +1,23 @@
-/** A small template for C programs.
- * Good for when I just want to write a quick test
+/** File Title
+ *  ==========
  *
+ *  Overall description of file
+ *
+ *  Last Modified: 2/14/14
+ *  Contributor(s): Nicholas Otto
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 
+/** Function Title 
+ *  --------------
+ *  Description of function
+ *
+ *  parameter1: description of parameter
+ *
+ *  returns: what it returns
+ */
 int main(int argc, char **argv){
 
     return 0;
