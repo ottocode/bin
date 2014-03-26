5c201ef77601ad477039cdddde9f4ff0e7b25343
Nicholas Otto <nicholas.d.otto@gmail.com>
2014-03-26 16:34:55 -0700

    adding default python file

---
+++ files/template.py
@@ -0,0 +1,22 @@
+### File Title
+ #  ========================================================================
+ #
+ #  Overall description of file
+ #
+ #
+ #  Last Modified: 3/17/14
+ #  Contributor(s): Nicholas Otto
+ ##
+
+
+### Function Title 
+ #  --------------
+ #  Description of function
+ #
+ #  parameter1: description of parameter
+ #
+ #  returns: what it returns
+ ##
+
+if __name__ == "__main__":
+    pass
